85cdf9203f472869356f973be284432fc62e014a
Jijo John <jijojohny358@gmail.com>
2022-08-08 20:32:15 +0530

    Adding Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
 import './App.css';
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
+
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+
+
 
 function App() {
   return (
     <>
+    <Router>
+
+
       
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="" element={<About />} />
+    </Routes>
+    </Router>
     </>
   );
-}
 
-export default App;
+} export default App;
